8406c6b589cd925030a90496f16c6c10d3f160c7
Victor Prata <victor.prata@buser.com.br>
2022-06-01 12:02:58 -0300

    Falta ex 19, 21, 24 e 27

--- secao_02_estrutura_de_decisao/ex_18_validador_de_data.py
+++ secao_02_estrutura_de_decisao/ex_18_validador_de_data.py
@@ -14,44 +14,97 @@ Faça um Programa que peça uma data no formato dd/mm/aaaa e determine se a mesm
     'Data válida'
     >>> validar_data('01/02/2004')
     'Data válida'
-    >>> validar_data('30/02/2004')
+    >>> validar_data('30/02/2004') 
     'Data inválida'
     >>> validar_data('01/13/2004')
     'Data inválida'
 
 """
-
+    #01/13/2004
 
 def validar_data(data: str):
     """Escreva aqui em baixo a sua solução"""
     data = data.split('/')
 
-    dia = int(data[0])
-    mes = int(data[1])
-    ano = int(data[2])
+    if len(data) == 3:
+        dia = int(data[0])
+        mes = int(data[1])
+        ano = int(data[2])
 
-    if dia > 31 or mes > 12: #TESTE MÊS E DIA IMPOSSIVEL
-        data = "'Data inválida'"
-    elif mes == 4 or mes == 6 or mes == 9 or mes == 11:
-        if dia > 30: #DIAS DE 30 DIAS
+        if dia > 31 or mes > 12 or ano == 0: #TESTE DIA, MÊS E ANO IMPOSSIVEL
             data = "'Data inválida'"
-    elif mes == 2:
-        if (ano % 4) == 0: 
-            if (ano % 100) != 0:
-                ano = True
-            elif (ano % 400) ==0:
-                ano = True
+        elif mes == 4 or mes == 6 or mes == 9 or mes == 11:
+            if dia > 30: #DIAS DE 30 DIAS
+                data = "'Data inválida'"
             else:
-                ano = False
-        else:
-            ano = False  
-
-        if ano == False:
-            data = "'Data inválida'" 
+                data = "'Data válida'"
+        elif mes == 2:
+            if dia > 29: #SE FEVEREIRO TEM MAIS DE 29 DIAS
+                data = "'Data inválida'"
+            #TESTE ANO BISSEXTO:    
+            elif dia == 29:
+                if (ano % 4) == 0 and (ano % 100) != 0:
+                    data = "'Data válida'"
+                    if (ano % 400) ==0:
+                        data = "'Data válida'"
+                    else:
+                        data = "'Data inválida'"
+                else:
+                    data = "'Data inválida'"
+            #TESTE DATA < 29 DIAS        
+            else:
+                data = "'Data válida'"
+            #FIM TESTE ANO BISSEXTO    
+            
     else:
-        data = "'Data inválida'" 
+        data = "'Data inválida'"
 
     print(data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # data = data.split('/')
+
+    # if len(data) == 3:
+    #     dia = int(data[0])
+    #     mes = int(data[1])
+    #     ano = int(data[2])
+
+    #     if dia > 31 or mes > 12 or ano == 0: #TESTE DIA, MÊS E ANO IMPOSSIVEL
+    #         data = "'Data inválida'"
+    #     elif mes == 4 or mes == 6 or mes == 9 or mes == 11:
+    #         if dia > 30: #DIAS DE 30 DIAS
+    #             data = "'Data inválida'"
+    #         else:
+    #             data = "'Data válida'"
+    #     elif mes == 2:
+    #         if dia > 29: #SE FEVEREIRO TEM MAIS DE 29 DIAS
+    #             data = "'Data inválida'"
+    #         else:
+    #             data = "'Data válida'"
+    # else:
+    #     data = "'Data inválida'"
+
+    # print(data)
 
 
 
