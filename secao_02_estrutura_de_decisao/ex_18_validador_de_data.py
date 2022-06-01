5c14d33a1a40c21d81aabd7c4a226efa275bf285
Victor Prata <victor.prata@buser.com.br>
2022-06-01 15:28:04 -0300

    21 e 21 Seção 2 faltando

--- secao_02_estrutura_de_decisao/ex_18_validador_de_data.py
+++ secao_02_estrutura_de_decisao/ex_18_validador_de_data.py
@@ -20,7 +20,6 @@ Faça um Programa que peça uma data no formato dd/mm/aaaa e determine se a mesm
     'Data inválida'
 
 """
-    #01/13/2004
 
 def validar_data(data: str):
     """Escreva aqui em baixo a sua solução"""
@@ -33,7 +32,7 @@ def validar_data(data: str):
 
         if dia > 31 or mes > 12 or ano == 0: #TESTE DIA, MÊS E ANO IMPOSSIVEL
             data = "'Data inválida'"
-        elif mes == 4 or mes == 6 or mes == 9 or mes == 11:
+        elif mes in (4, 6, 9, 11):
             if dia > 30: #DIAS DE 30 DIAS
                 data = "'Data inválida'"
             else:
@@ -60,95 +59,4 @@ def validar_data(data: str):
         data = "'Data inválida'"
 
     print(data)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    # data = data.split('/')
-
-    # if len(data) == 3:
-    #     dia = int(data[0])
-    #     mes = int(data[1])
-    #     ano = int(data[2])
-
-    #     if dia > 31 or mes > 12 or ano == 0: #TESTE DIA, MÊS E ANO IMPOSSIVEL
-    #         data = "'Data inválida'"
-    #     elif mes == 4 or mes == 6 or mes == 9 or mes == 11:
-    #         if dia > 30: #DIAS DE 30 DIAS
-    #             data = "'Data inválida'"
-    #         else:
-    #             data = "'Data válida'"
-    #     elif mes == 2:
-    #         if dia > 29: #SE FEVEREIRO TEM MAIS DE 29 DIAS
-    #             data = "'Data inválida'"
-    #         else:
-    #             data = "'Data válida'"
-    # else:
-    #     data = "'Data inválida'"
-
-    # print(data)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    # if dia <= 31: 
-    #     if mes == 2 or mes == 4 or mes == 6 or mes == 9 or mes == 11: #MESES 30 DIAS OU FEVEREIRO
-    #         if dia > 30:
-    #             data = "'Data inválida'"
-    #         elif dia == 29:
-    #             #TESTANDO ANO BISSEXTO:
-    #             if (ano % 4) == 0: 
-    #                 if (ano % 100) != 0:
-    #                     ano = True
-    #                 elif (ano % 400) ==0:
-    #                     ano = True
-    #                 else:
-    #                     ano = False
-    #             else:
-    #                 ano = False
-    #             #FIM TESTE ANO BISSEXTO
-    #             if ano == False:
-    #                data = "'Data inválida'"
-    #             else: 
-    #                 data = "'Data válida'"
-    #    else:
-    #         data = 'Data válida'
-    # else:
-    #     data = 'Data inválida'
-    
-    # if mes > 12:
-    #     data = "'Data inválida'"
 
--- secao_02_estrutura_de_decisao/ex_19_decomposicao_de_numero.py
+++ secao_02_estrutura_de_decisao/ex_19_decomposicao_de_numero.py
@@ -47,25 +47,72 @@ Observando os termos no plural a colocação do "e", da vírgula entre outros. E
     >>> decompor_numero(7)
     '7 = 7 unidades'
 
-"""
+""" 
 
 
 def decompor_numero(numero: int):
     """Escreva aqui em baixo a sua solução"""
-    if numero > 1000:
+    numero_inicial = numero
+    if numero >= 1000:
         print("'O número precisa ser menor que 1000'")
     elif numero < 0:
         print("'O número precisa ser positivo'")
     else:
-        #TESTAR CENTENA
-        if numero / 100 == 1:
-            centena = 1
-        elif (numero / 100) <= 9:
-            centena = int(numero / 100) #armazena dígito da centena
-    
-    if centena == 1:
-        str_centena = 'centena'
-    else:
-        str_centena = 'centenas'
+        centena = numero // 100
+        numero_sem_centena = numero - centena*100
+        dezena = numero_sem_centena // 10
+        unidade = numero_sem_centena - dezena*10
 
-    print(f"'{numero} = {centena} {str_centena}'")
+        #USO DO S
+        if centena > 1:
+            plural_centena = 's'
+        else:
+            plural_centena = ''
+
+        if dezena > 1:
+            plural_dezena = 's'
+        else:
+            plural_dezena = ''
+
+        if unidade > 1:
+            plural_unidade = 's'
+        else:
+            plural_unidade = ''
+        #FIM USO S
+        if centena > 0:
+            str_centena = (f"{centena} centena{plural_centena}")
+        else:
+            str_centena = ''
+
+        if dezena > 0:
+            str_dezena = (f"{dezena} dezena{plural_dezena}")
+        else:
+            str_dezena = ''
+
+        if unidade > 0:
+            str_unidade = (f"{unidade} unidade{plural_unidade}")
+        else:
+            str_unidade = ''
+        #---------------------OUTPUT------------------------------------------------------
+        if centena > 0 and dezena > 0 and unidade > 0:
+            print(f"'{numero_inicial} = {str_centena}, {str_dezena} e {str_unidade}'")
+
+        elif centena == 0 and dezena > 0 and unidade > 0:
+            print(f"'{numero_inicial} = {str_dezena} e {str_unidade}'")
+        
+        elif centena > 0 and dezena == 0 and unidade > 0:
+            print(f"'{numero_inicial} = {str_centena} e {str_unidade}'")
+
+        elif centena > 0 and dezena > 0 and unidade == 0:
+            print(f"'{numero_inicial} = {str_centena} e {str_dezena}'")
+
+        elif centena > 0 and dezena == 0 and unidade == 0:
+            print(f"'{numero_inicial} = {str_centena}'")
+
+        elif centena == 0 and dezena > 0 and unidade == 0:
+            print(f"'{numero_inicial} = {str_dezena}'")
+
+        elif centena == 0 and dezena == 0 and unidade > 0:
+            print(f"'{numero_inicial} = {str_unidade}'")
+
+
