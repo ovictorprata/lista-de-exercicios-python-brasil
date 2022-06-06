642eb25a2f1370c726512a8badd2c44f9079e930
Victor Prata <victor.prata@buser.com.br>
2022-06-06 15:04:02 -0300

    S03E10 OK

--- secao_03_estrutura_de_repeticao/ex_09_numeros_impares_de_1_a_50.py
+++ secao_03_estrutura_de_repeticao/ex_09_numeros_impares_de_1_a_50.py
@@ -12,3 +12,6 @@ Faça um programa que imprima na tela apenas os números ímpares entre 1 e 50
 
 def calcular_numeros_impares_de_1_a_50() -> str:
     """Escreva aqui em baixo a sua solução"""
+    i = 0
+    lista = list(range(1, 50, 2))
+    print("'" + ', '.join(map(str, lista)) + "'") 
--- secao_03_estrutura_de_repeticao/ex_10_gerar_numeros_de_intervalo.py
+++ secao_03_estrutura_de_repeticao/ex_10_gerar_numeros_de_intervalo.py
@@ -16,3 +16,8 @@ Faça um programa que receba dois números inteiros e gere os números inteiros 
 
 def calcular_numeros_no_intervalo(inicio: int, fim: int) -> str:
     """Escreva aqui em baixo a sua solução"""
+    lista = []
+    while inicio < fim: 
+        lista.append(inicio)
+        inicio += 1
+    print("'" + ', '.join(map(str, lista)) + "'")
