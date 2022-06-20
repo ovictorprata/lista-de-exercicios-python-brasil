b3c371c381989862169bf0826e005daa4fa8c1bb
Victor Prata <victor.prata@buser.com.br>
2022-06-20 14:34:55 -0300

    S03 OK

--- secao_03_estrutura_de_repeticao/ex_15_fibonnacci_ate_n.py
+++ secao_03_estrutura_de_repeticao/ex_15_fibonnacci_ate_n.py
@@ -25,16 +25,55 @@ n−ésimo termo.
 
 def calcular_serie_de_fibonacci(n: int) -> str:
     """Escreva aqui em baixo a sua solução"""
-    lista = [1,1]
+"""
+Exercício 15 da seção de estrutura sequencial da Python Brasil:
+https://wiki.python.org.br/EstruturaDeRepeticao
+
+A série de Fibonacci é formada pela seqüência 1,1,2,3,5,8,13,21,34,55,... Faça um programa capaz de gerar a série até o
+n−ésimo termo.
+
+    >>> calcular_serie_de_fibonacci(1)
+    '1'
+    >>> calcular_serie_de_fibonacci(2)
+    '1, 1'
+    >>> calcular_serie_de_fibonacci(3)
+    '1, 1, 2'
+    >>> calcular_serie_de_fibonacci(4)
+    '1, 1, 2, 3'
+    >>> calcular_serie_de_fibonacci(5)
+    '1, 1, 2, 3, 5'
+    >>> calcular_serie_de_fibonacci(6)
+    '1, 1, 2, 3, 5, 8'
+    >>> calcular_serie_de_fibonacci(7)
+    '1, 1, 2, 3, 5, 8, 13'
+
+"""
+
+
+def calcular_serie_de_fibonacci(n: int) -> str:
+    """Escreva aqui em baixo a sua solução"""
+
+    [1, 1, 2, 3, 5, 8, 13, 21, 34]
+
+    fibonacci = [1, 1]
     i = 2
+
     if n == 1:
         print("'1'")
     else:
         while i < n:
-            nesimo = lista[-1] + lista[-2]
-            lista.append(nesimo)
+            fibonacci.append(fibonacci[-2] + fibonacci[-1])
             i += 1
-        print("'" + ', '.join(map(str, lista)) + "'")
+        
+        print("'" + ', '.join(map(str, fibonacci)) + "'")
+
+
     
 
 
+
+    
+
+ 
+
+    
